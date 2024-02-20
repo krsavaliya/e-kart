b59eac5465ec5d38ca60fae5e2893db1c24019be
Keval Savaliya <kevalsavaliya2519@gmail.com>
2024-02-20 21:54:55 +0530

    Offer Page css responsive

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import './offers.css'
-import exclusiveimg from '../Assets/exclusive_image.jpeg'
+import React from "react";
+import "./offers.css";
+import exclusiveimg from "../Assets/exclusive_image.jpeg";
 
 export const Offers = () => {
   return (
-    <div className='offers'>
+    <div className="offers">
       <div className="offers-left">
         <h1>Exclusive</h1>
         <h1>Offers For You</h1>
@@ -12,9 +12,9 @@ export const Offers = () => {
         <button>check now</button>
       </div>
       <div className="offers-right">
-        <img src={exclusiveimg} alt="" height="375" width="548"/>
-      </div> 
+        <img src={exclusiveimg} alt="" height="375" width="548" />
+      </div>
     </div>
-  )
-}
+  );
+};
 export default Offers;
